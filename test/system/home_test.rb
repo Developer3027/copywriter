bdebb52c0899f5e27c560a4efbec10a7a7d4e799
Mason Roberts <mrobe18@gmail.com>
2024-11-22 08:58:07 -0500

    added tests for home links

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -3,15 +3,15 @@ require "application_system_test_case"
 class HomeTest < ApplicationSystemTestCase
   # Test home page for specific visuals
   # 11 assertions run for this test on root
-  test "visiting home" do
+  test "verify home content" do
     visit root_url
 
     # Ensure there are two links for LKM Creative on page
     assert_selector "a", exact_text: "LKM Creative", count: 2
     # Ensure the phrase "Writing WORDS that SELL!"
     assert_selector "span", exact_text: "Writing WORDS that SELL!"
-    # Ensure the phrase "We write copy that sells like toilet paper in a pandemic. Could you be our next success story?"
-    assert_selector "p", exact_text: "We write copy that sells like toilet paper in a pandemic. Could you be our next success story?"
+    # Ensure the phrase "We write copy that sells like toilet paper in a pandemic."
+    assert_selector "p", text: "We write copy that sells like toilet paper in a pandemic."
     # Verify owner name
     assert_selector "h2", text: "Lauren Gouws"
     # Verify producer name
@@ -25,8 +25,22 @@ class HomeTest < ApplicationSystemTestCase
     # Ensure there is youtube video
     assert_selector "iframe[src*='https://www.youtube.com/embed/RBtq_s91KAE?si=qHs3HFoKXE8eaaWW']"
     # Ensure services still listed
-    assert_selector "p", text: "website copy to brochure copy, blogs, articles, crowdfunding campaign copy, billboards, video scripts, TV ads, radio spots, explainer videos, mailers,"
+    assert_selector "p", text: "website copy to brochure copy, blogs, articles, crowdfunding campaign copy, billboards,"
     # Verify Shawn email on page
     assert_selector "p", text: "shaun@lkmcreative.com"
+  end
+
+  # Test learn more link on the home page
+  test "verify learn more link" do
+    visit root_url
+    click_on "Learn More -"
+    assert_selector "h1", text: "About Us"
+  end
+
+  # Test the team link on the home page
+  test "verify the team link" do
+    visit root_url
+    click_on "The Team -"
+    assert_selector "h2", text: "Meet the Team"
   end
 end
