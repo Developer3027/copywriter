1c01d9c89614065a357bc2e4f5d8d0d082c66113
Mason Roberts <mrobe18@gmail.com>
2024-11-25 21:27:01 -0500

    commenting out failed testing so removed enum from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # enum role: { admin: 0, user: 1 }
 end
